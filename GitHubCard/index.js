3af535931e71992f40ac9ff263ebcd93b15ae1d5
Josiah Roa <josiahroa18@gmail.com>
2020-02-13 13:25:45 -0700

    Finished add card component and populatin with data from get request

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,31 @@
            https://api.github.com/users/<your name>
 */
 
+let cards = document.querySelector('.cards');
+axios.get('https://api.github.com/users/josiahroa18')
+.then(res => {
+  cards.appendChild(GitHubCard(res.data));
+})
+.catch(err => {
+  console.log(err);
+})
+
+axios.get('https://api.github.com/users/josiahroa18/followers')
+.then(res => {
+  res.data.forEach(user => {
+    axios.get(user.url)
+    .then(res => {
+      cards.appendChild(GitHubCard(res.data));
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  })
+})
+.catch(err => {
+  console.log(err);
+})
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +49,8 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [];
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +71,53 @@ const followersArray = [];
 </div>
 
 */
+
+function GitHubCard(data){
+  // Create elements
+  const card = document.createElement('div'),
+        img = document.createElement('img'),
+        info = document.createElement('div'),
+        name = document.createElement('h3'),
+        username = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        link = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
+
+  // Nest Elements
+  card.appendChild(img);
+  card.appendChild(info);
+  info.append(name, username, location, profile, followers, following, bio);
+
+  // Apply classes
+  card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  // Apply text context
+  name.textContent = data.name;
+  username.textContent = data.login;
+  location.textContent = data.location;
+  profile.textContent = 'Profile: ';
+  link.textContent = data.html_url;
+  followers.textContent = `Followers: ${data.followers}`;
+  following.textContent = `Following: ${data.following}`;
+  bio.textContent = `Bio: ${data.bio}`;
+
+  // Apply attributes
+  img.src = data.avatar_url;
+  link.href = data.html_url;
+
+  // Nesting link after applying text context to profile
+  profile.appendChild(link);
+  
+  return card;
+}
+
+// console.log(GitHubCard());
 
 /* List of LS Instructors Github username's: 
   tetondan
